7b278f8fef0063b4ac9f1d0a7c60e4b69ef2e250
Alex Teisheira <fukirx@gmail.com>
2020-11-25 22:23:03 +0200

    prod config6

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 import "./resources/vars.sass";
