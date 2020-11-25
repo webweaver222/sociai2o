14ff916b7f7fa2f793e9c4949b7445a2acce312d
Alex Teisheira <fukirx@gmail.com>
2020-11-25 20:56:43 +0200

    prod config5

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import "./resources/vars.sass";
 import "./resources/reset.sass";
@@ -15,13 +16,17 @@ import App from "./components/app";
 import diviaiService from "./services/diviaiApi";
 import store from "./store";
 
+const history = createBrowserHistory({
+  basename: "sociai",
+});
+
 const service = new diviaiService();
 
 ReactDom.render(
   <Provider store={store}>
     <ErrorBoundry>
       <ServiceProvider value={service}>
-        <Router>
+        <Router history={history}>
           <App />
         </Router>
       </ServiceProvider>
